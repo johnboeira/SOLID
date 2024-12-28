8b313db7cc31d617f9a5623fdfd2b4e789a9df85
johnboeira <johnboeira@gmail.com>
2024-12-27 21:52:51 -0300

    refactor: Projeto inicial

--- Rating.ConsoleApp/Program.cs
+++ Rating.ConsoleApp/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Rating.ConsoleApp;
+﻿namespace ArdalisRating;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Insurance Rating System Starting...");
+
+        var engine = new RatingEngine();
+        engine.Rate();
+
+        if (engine.Rating > 0)
+        {
+            Console.WriteLine($"Rating: {engine.Rating}");
+        }
+        else
+        {
+            Console.WriteLine("No rating produced.");
+        }
     }
 }
