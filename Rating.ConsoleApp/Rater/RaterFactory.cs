764753330b89043c94850407efcc60fb45b738a2
johnboeira <johnboeira@gmail.com>
2025-02-07 18:57:08 -0300

    refactor: Usando OCP e Reflection

---
+++ Rating.ConsoleApp/Rater/RaterFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rating.ConsoleApp.Rater;
+
+public class RaterFactory
+{
+    public Rater Create(Policy policy, RatingEngine engine)
+    {
+        try
+        {
+            string currentNamespace = typeof(RaterFactory).Namespace;
+
+            return (Rater)Activator.CreateInstance(
+                Type.GetType($"{currentNamespace}.{policy.Type}PolicyRater"),
+                    new object[] { engine, engine.Logger });
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
